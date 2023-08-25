86415e90d58c9c9b0ed145189695f68be6d20c23
Gleison Batista <gleisonbs@gmail.com>
2023-08-24 23:05:33 -0300

    tests: add user controller tests

--- test/users/users.controller.spec.ts
+++ test/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from '../../src/users/users.controller';
 import { UsersService } from '../../src/users/users.service';
 import { AuthService } from '../../src/users/auth.service';
 import { User } from '../../src/users/users.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -28,7 +29,11 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: async (email: string, password: string) => ({
+        id: 1,
+        email,
+        password,
+      }),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -46,5 +51,35 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('finaAll returns a lits of user with the given email', () => {});
+  it('findAllUsers returns a lits of user with the given email', async () => {
+    const allUsers = await controller.findAllUsers('test@email.com');
+    expect(allUsers.length).toEqual(1);
+    expect(allUsers[0].email).toEqual('test@email.com');
+  });
+
+  it('findUser returns a user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+    expect(user.id).toEqual(1);
+  });
+
+  it('findUser throws if no user with the given id is found', async () => {
+    jest.spyOn(fakeUsersService, 'findOne').mockResolvedValueOnce(null);
+    const user = controller.findUser('1000');
+    expect(user).rejects.toThrow(new NotFoundException('user not found'));
+  });
+
+  it('Signin updates session object and returns user', async () => {
+    const session: { userId: number } = { userId: undefined };
+    const user = await controller.signin(
+      {
+        email: 'test@email.com',
+        password: 'test.password',
+      },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
 });
